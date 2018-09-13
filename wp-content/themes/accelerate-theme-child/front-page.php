5d42d03597afe7a0cb735f147f8894fe759d4f26
Jason Thomas <nanoman689@gmail.com>
2018-09-12 19:27:58 -0600

    Updated front-page.php

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -1,8 +1,6 @@
 <?php
 /**
  * The template for displaying the homepage
- * 
- * Child Theme
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
@@ -11,34 +9,33 @@
  *
  * @package WordPress
  * @subpackage Accelerate Marketing
- * @since Accelerate Marketing 1.0
+ * @since Accelerate Marketing 2.0
  */
-
 get_header(); ?>
 
-
-<section class="home-page">
-	<div class="site-content">
-		<?php while ( have_posts() ) : the_post(); ?>
-			<div class='homepage-hero'>
+	<div id="primary" class="home-page hero-content">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
-				<a class="button" href="<?php echo home_url(); ?>/blog">View Our Work</a>
+					<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+	</div><!-- #primary -->
+
+	<!-- RECENT BLOG POST -->
+	<section class="recent-posts">
+		<div class="site-content">
+			<div class="blog-post">
+				<h4>From the Blog</h4>
+				<?php query_posts('posts_per_page=1'); ?>
+					<!-- the loop -->
+					<?php while ( have_posts() ) : the_post(); ?>
+						<h3><?php the_title(); ?></h3>
+						<?php the_excerpt(); ?>
+					<?php endwhile; // end of the loop. ?>
+				<?php wp_reset_query(); // resets the altered query back to the original ?>
 			</div>
-			<section class="recent-posts">
- 				<div class="site-content">
- 				<div class="blog-post">
- 					<h4>From the Blog</h4>
- 					<?php query_posts('posts_per_page=1'); ?>
-  						<?php while ( have_posts() ) : the_post(); ?>
-    						<h3><?php the_title(); ?></h3>
-    						<?php the_excerpt(); ?> 
-  						<?php endwhile; ?> 
-					<?php wp_reset_query(); ?>
- 				</div>	
- 				</div>
-			</section>
-		<?php endwhile; // end of the loop. ?>
-	</div><!-- .container -->
-</section><!-- .home-page -->
+		</div>
+	</section>
 
 <?php get_footer(); ?>
